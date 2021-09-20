4e4d322e2ccfa51d49195f9f0775144003f76ddd
Galyna <77695117+GalkaNH@users.noreply.github.com>
2021-09-20 14:15:38 +0200

    Add files via upload

---
+++ my_script/hello_3.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+OUTPUT=hello_new
+
+echo "$OUTPUT" > /d/1_DOKUMENTY/nauka__jesien/my_script/"${OUTPUT}".sh
+
---
+++ my_script/private_file.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+OUTPUT=this_file_is_private
+
+echo "$OUTPUT" > /d/1_DOKUMENTY/nauka__jesien/my_script/"${OUTPUT}".txt
